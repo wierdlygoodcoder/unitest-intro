41054fd61018bcbf89d2b5253f0f6589e4dd0104
wierdlygoodcoder <ashleyrolfe954@msn.com>
2022-10-07 21:26:13 +0000

    mocking our method

--- student.py
+++ student.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+import requests
 
 
 class Student:
@@ -17,11 +18,19 @@ class Student:
 
     @property
     def email(self):
-        return f"{self._first_name.lower()}\
-.{self._last_name.lower()}@email.com"
+        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
 
     def alert_santa(self):
         self.naughty_list = True
 
     def apply_extension(self, days):
-        self.end_date = self.end_date + timedelta(days=days)
+        self.end_date += timedelta(days=days)
+
+    def course_schedule(self):
+        response = requests.get(
+            f"https://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong with the request"
