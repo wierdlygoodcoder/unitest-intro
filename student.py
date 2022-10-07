6ccccda028cb3fa56780241e4695de3413e3b335
wierdlygoodcoder <ashleyrolfe954@msn.com>
2022-10-07 12:08:10 +0000

    Code-Along Challenge

--- student.py
+++ student.py
@@ -22,3 +22,6 @@ class Student:
 
     def alert_santa(self):
         self.naughty_list = True
+
+    def apply_extension(self, days):
+        self.end_date = self.end_date + timedelta(days=days)
--- test_student.py
+++ test_student.py
@@ -1,5 +1,6 @@
 import unittest
 from student import Student
+from datetime import timedelta
 
 
 class TestStudent(unittest.TestCase):
@@ -31,6 +32,19 @@ class TestStudent(unittest.TestCase):
     def test_email(self):
         print('test_email')
         self.assertEqual(self.student.email, "john.doe@email.com")
+
+    def test_apply_extension(self):
+        print('test_apply_extension')
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
+        """
+        The method below is also great!  But keep in mid that  it will
+        only be correct if a student has received 1 extenstion.  If
+        they receive a second - it would return false
+        self.student.apply_extension(5)
+        self.assertEqual(self.student.end_date, self.student._start_date + timedelta(days=370))
+        """
 
 
 if __name__ == "__main__":
