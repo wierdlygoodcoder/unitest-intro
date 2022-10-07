58557a3a3640e9f0d24385827862558ded93eecd
wierdlygoodcoder <ashleyrolfe954@msn.com>
2022-10-07 11:42:09 +0000

    Building a method using TDD - creating the code

--- student.py
+++ student.py
@@ -2,16 +2,22 @@ from datetime import date, timedelta
 
 
 class Student:
-
-    """ A Student class as base for method testing """
+    """ A Student class as a basis for method testing """
 
     def __init__(self, first_name, last_name):
         self._first_name = first_name
         self._last_name = last_name
         self._start_date = date.today()
-        self._end_date = date.today() + timedelta(days=365)
+        self.end_date = date.today() + timedelta(days=365)
         self.naughty_list = False
 
     @property
     def full_name(self):
         return f"{self._first_name} {self._last_name}"
+
+    @property
+    def email(self):
+        return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
+
+    def alert_santa(self):
+        self.naughty_list = True
--- test_student.py
+++ test_student.py
@@ -3,18 +3,19 @@ from student import Student
 
 
 class TestStudent(unittest.TestCase):
-
     def test_full_name(self):
-        student = Student('John', 'Doe')
-
-        self.assertEqual(student.full_name, 'John Doe')
+        student = Student("John", "Doe")
+        self.assertEqual(student.full_name, "John Doe")
 
     def test_alert_santa(self):
-        student = Student('John', 'Doe')
+        student = Student("John",  "Doe")
         student.alert_santa()
-
         self.assertTrue(student.naughty_list)
 
+    def test_email(self):
+        student = Student("John",  "Doe")
+        self.assertEqual(student.email, "john.doe@email.com")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
